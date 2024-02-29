70e9b59e528f061eeeda4bbbea58b4a37e674b79
algarfer <algarferx@proton.me>
2024-02-29 23:26:35 +0100

    Added logo to nav bar and other minor changes
    Nav module code could be improved

--- webapp/src/components/Nav.jsx
+++ webapp/src/components/Nav.jsx
@@ -11,8 +11,8 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import {Link} from "react-router-dom";
+import {ReactComponent as CustomIcon} from "../media/logoS.svg";
 
 const pages = [
     { displayed: 'Home', link: "/home" },
@@ -28,9 +28,94 @@ const settings = [
 ];
 
 function Nav() {
-    
+
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const threshold = 899; // xs size
+
+    const NavIcon = () => {
+        const isWideScreen = width > threshold;
+        return (
+            <div style={{ margin: isWideScreen ? "1rem 2rem" : "1rem auto" }}>
+                <CustomIcon />
+            </div>
+        );
+    }
+
+    const DropDownMenu = () => {
+        return (<Box sx={{flexGrow: {md: 1}, display: {xs: 'flex', md: 'none'}, width: {xs: 'fit-content'}}}>
+
+            <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+            >
+                <MenuIcon/>
+            </IconButton>
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                    display: {xs: 'block', md: 'none'},
+                }}
+            >
+                {
+                    pages.map((page) => (
+                        <MenuItem
+                            key={page.displayed}
+                            onClick={handleCloseNavMenu}
+                            component={Link}
+                            to={page.link}>
+                            <Typography textAlign="center">
+                                {page.displayed}
+                            </Typography>
+                        </MenuItem>
+                    ))
+                }
+            </Menu>
+        </Box>)
+    }
+
+    const NavMenu = () => {
+        return (<Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+            {pages.map((page) => (
+                <Button
+                    key={page.displayed}
+                    onClick={handleCloseNavMenu}
+                    component={Link}
+                    sx={{my: 2, color: 'white', display: 'block'}}
+                    to={page.link}
+                >
+                    {page.displayed}
+                </Button>
+            ))}
+        </Box>)
+    }
+
+    React.useEffect(() => {
+        const handleResizeWindow = () => setWidth(window.innerWidth);
+        // subscribe to window resize event "onComponentDidMount"
+        window.addEventListener("resize", handleResizeWindow);
+        return () => {
+            // unsubscribe "onComponentDestroy"
+            window.removeEventListener("resize", handleResizeWindow);
+        };
+    }, [])
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -51,76 +136,17 @@ function Nav() {
         <AppBar position="sticky">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {
-                                pages.map((page) => (
-                                    <MenuItem 
-                                        key={page.displayed}
-                                        onClick={handleCloseNavMenu}
-                                        component={Link}
-                                        to={page.link}>
-                                        <Typography textAlign="center">
-                                            {page.displayed}
-                                        </Typography>
-                                    </MenuItem>
-                                ))
-                            }
-                        </Menu>
-                    </Box>
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page.displayed}
-                                onClick={handleCloseNavMenu}
-                                component={Link}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                                to={page.link}
-                            >
-                                {page.displayed}
-                            </Button>
-                        ))}
-                    </Box>
-
-                    <Box sx={{ flexGrow: 0 }}>
+                    <DropDownMenu />
+                    <NavIcon />
+                    <NavMenu />
+                    <Box sx={{flexGrow: 0}}>
                         <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Jordi Hurtado" src="https://i.imgur.com/rnFjdQJ.jpeg" />
+                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                <Avatar alt="Jordi Hurtado" src="https://i.imgur.com/rnFjdQJ.jpeg"/>
                             </IconButton>
                         </Tooltip>
                         <Menu
-                            sx={{ mt: '45px' }}
+                            sx={{mt: '45px'}}
                             id="menu-appbar"
                             anchorEl={anchorElUser}
                             anchorOrigin={{
@@ -136,7 +162,8 @@ function Nav() {
                             onClose={handleCloseUserMenu}
                         >
                             {settings.map((setting) => (
-                                <MenuItem key={setting.displayed} onClick={handleCloseUserMenu} component={Link} to={setting.link}>
+                                <MenuItem key={setting.displayed} onClick={handleCloseUserMenu} component={Link}
+                                          to={setting.link}>
                                     <Typography textAlign="center">{setting.displayed}</Typography>
                                 </MenuItem>
                             ))}
