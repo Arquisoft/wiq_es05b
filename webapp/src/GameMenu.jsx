fed63c8b5493b76b22c9f6a269df652bdfa5b544
algarfer <algarferx@proton.me>
2024-03-05 19:48:38 +0100

    Game menu improved

--- webapp/src/GameMenu.jsx
+++ webapp/src/GameMenu.jsx
@@ -1,31 +1,44 @@
-import {Button, Container, Typography} from "@mui/material";
+import {Button, Container, Divider, Paper, Typography} from "@mui/material";
+
+const buttonConfig = {
+    width: "7rem"
+}
+
+const buttonGroup = {display: "flex", justifyContent: "space-evenly", margin: "1rem 0"}
+
+const MyButton = ({text}) => {
+    return (
+        <Button variant="contained" sx={buttonConfig}>{text}</Button>
+    )
+}
 
 export default function GameMenu() {
 
     return (
-        <Container>
-            <Typography>
-                Menu
-            </Typography>
-            <Container>
-                <Typography>
-                    Options
+        <Container component="main" maxWidth="md" sx={{ marginTop: 4, display: "flex", flexDirection: {xs:"row", md:"column"} }}>
+            <Paper elevation={3} sx={{margin: "2rem 0", padding: "1rem", textAlign: "center"}}>
+                <Typography variant="h3" component="p" sx={{marginBottom: "2rem"}}>
+                    Menu
                 </Typography>
                 <Container>
-                    <Button>Time</Button>
-                    <Button>Custom</Button>
+                    <Typography variant="h5" component="p">
+                        Options
+                    </Typography>
+                    <Container sx={buttonGroup}>
+                        <MyButton text="Time" />
+                        <MyButton text="Custom" />
+                    </Container>
                 </Container>
-            </Container>
-            <Container>
-                <Typography>Choose a category to play</Typography>
                 <Container>
-                    <Button>Countries</Button>
-                    <Button>Other category</Button>
-                    <Button>Other category</Button>
-                    <Button>Other category</Button>
+                    <Typography variant="h5" component="p">Choose a category to play</Typography>
+                    <Container sx={buttonGroup}>
+                        <MyButton text="Countries" />
+                        <MyButton text="Other Category" />
+                        <MyButton text="Other Category" />
+                        <MyButton text="Other Category" />
+                    </Container>
                 </Container>
-            </Container>
-            <Button>Play</Button>
+            </Paper>
         </Container>
     )
 }
