7181966a3d4380cea1171f518680e55ec21e3c84
algarfer <algarferx@proton.me>
2024-03-31 18:25:05 +0200

    Added middleware and remaining history endpoints to gateway

--- gatewayservice/routes/historyRoutes.js
+++ gatewayservice/routes/historyRoutes.js
@@ -4,16 +4,8 @@ module.exports = (app, axios, errorHandler) => {
   app.get("/history/get/:userId", (req, res) => {
     const { userId } = req.params
     const { page, limit } = req.query
-    const { userId: userIdBody } = req.body
 
-    // TODO - Debug
-    // if (userIdBody && userIdBody !== userId) {
-    //   res.status(401).json({ error: "You don't own this save" })
-    //   return
-    // }
-
-    // let url = `${historyService}/get/${userId}`
-    let url = `${historyService}/get/${userIdBody}`
+    let url = `${historyService}/get/${userId}`
 
     if(!isNaN(parseInt(page)) && !isNaN(parseInt(limit))) {
       url += `?page=${page}&limit=${limit}`
@@ -28,17 +20,32 @@ module.exports = (app, axios, errorHandler) => {
 
   app.get("/history/get/:userId/:id", (req, res) => {
     const { userId, id } = req.params
-    const { userId: userIdBody } = req.body
-
-    if (userIdBody && userIdBody !== userId) {
-      res.status(401).json({ error: "You don't own this save" })
-      return
-    }
 
     axios
       .get(`${historyService}/get/${userId}/${id}`)
       .then(response => res.status(response.status).json(response.data))
       .catch(error =>
         errorHandler(error, res, "An error occurred while fetching user history"))
+  })
+
+  app.post("/history/create", (req, res) => {
+    const { userId, category } = req.body
+
+    axios
+      .post(`${historyService}/create`, { userId, category })
+      .then(response => res.status(response.status).json(response.data))
+      .catch(error =>
+        errorHandler(error, res, "An error occurred while creating the save"))
+  })
+
+  app.post("/history/add/:id", (req, res) => {
+    const { id } = req.params
+    const { last, statement, options, answer, correct, time, points } = req.body
+
+    axios
+      .post(`${historyService}/add/${id}`, { last, statement, options, answer, correct, time, points })
+      .then(response => res.status(response.status).json(response.data))
+      .catch(error =>
+        errorHandler(error, res, "An error occurred while creating the save"))
   })
 }
