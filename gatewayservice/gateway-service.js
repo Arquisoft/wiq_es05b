eb851031724c14c8372b3e10a3680a14760a8b9d
algarfer <algarferx@proton.me>
2024-04-02 01:11:52 +0200

    Disabled https on the webapp

--- gatewayservice/gateway-service.js
+++ gatewayservice/gateway-service.js
@@ -6,9 +6,7 @@ const axios = require('axios');
 const app = express();
 const port = 8000;
 
-app.use(cors({
-  origin: "https://cyt.is-cool.dev/"
-}));
+app.use(cors());
 app.use(express.json());
 
 //Prometheus configuration
