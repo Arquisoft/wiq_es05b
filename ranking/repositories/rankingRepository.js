78562d024e04396fe4d3482796c128f186d6f72b
dononitram <uo288787@uniovi.es>
2024-04-09 18:40:45 +0200

    ranking running on mongo

--- ranking/repositories/rankingRepository.js
+++ ranking/repositories/rankingRepository.js
@@ -1,63 +1,50 @@
-const fs = require('node:fs');
+
 module.exports = {
-    client: null,
+    
+    mongoose: null,
     uri: null,
+    collectionName: 'questions',
+    Record: require("../ranking-model"),
 
-    getConnection: async function () {
-        let connection = new this.client(this.uri)
-        await connection.connect()
-        return connection
-    },
-
-    init: async function (client, uri) {
-        this.client = client
-        this.uri = uri
-        let connection
-        try {
-            connection = await this.getConnection()
-            let exists = await connection.query("SELECT EXISTS (SELECT 1 FROM "
-                + "information_schema.tables WHERE table_name = 'records') "
-                + "AS table_existence;")
-            if (!exists.rows[0].table_existence) {
-                let data = fs.readFileSync('schema.sql', 'utf8')
-                console.log('Table not found, creating records database')
-                await connection.query(data)
-            }
-        } catch (error) {
-            throw error.code
-        } finally {
-            connection && await connection.end()
-        }
+    init: function(mongoose, uri) {
+        this.mongoose = mongoose;
+        this.uri = uri;
     },
 
     getRanking: async function (n) {
-        let connection
         try {
-            connection = await this.getConnection()
-            let result = await connection
-                .query('SELECT * FROM records ORDER BY points DESC LIMIT $1',
-                    [n]);
-            return result.rows
+            await this.mongoose.connect();
+            let result = this
+                .mongoose
+                .connection
+                .collection(this.collectionName)
+                .find()
+            return result.rows;
         } catch (error) {
-            throw error.code
+            throw error.message
         } finally {
-            connection && await connection.end()
+            await this.mongoose.disconect();
         }
     },
 
     insertRecord: async function (username, points) {
-        let connection
         try {
-            connection = await this.getConnection()
-            let result = await connection
-                .query('INSERT INTO records (name, points, date) VALUES ($1, $2, $3)',
-                    [username, points, new Date()]);
-            console.log('Score successfully stored')
-            return result.rows
+
+            await this.mongoose.connect();
+
+            const record = new this.Record({
+                name: username,
+                points: points
+            });
+
+            record.save();
+
+            return { message: 'Record inserted' };
+
         } catch (error) {
-            throw error.code
+            throw error.message;
         } finally {
-            connection && connection.end()
+            await this.mongoose.disconnect();
         }
     }
 }
