3592609e3030cb21c1f489d049cd8c4c45986476
algarfer <algarferx@proton.me>
2024-04-15 02:30:36 +0200

    Added new user and configured the nginx log format

--- logging/setup/start.sh
+++ logging/setup/start.sh
@@ -13,6 +13,7 @@ declare -A users_passwords
 users_passwords=(
 	[logstash_internal]="${LOGSTASH_INTERNAL_PASSWORD:-}"
 	[kibana_system]="${KIBANA_SYSTEM_PASSWORD:-}"
+	["${CUSTOM_USER:-user}"]="${CUSTOM_USER_PASSWORD:-}"
 #	[metricbeat_internal]="${METRICBEAT_INTERNAL_PASSWORD:-}"
 #	[filebeat_internal]="${FILEBEAT_INTERNAL_PASSWORD:-}"
 #	[heartbeat_internal]="${HEARTBEAT_INTERNAL_PASSWORD:-}"
@@ -23,6 +24,7 @@ users_passwords=(
 declare -A users_roles
 users_roles=(
 	[logstash_internal]='logstash_writer'
+	["${CUSTOM_USER:-user}"]="kibana_admin"
 #	[metricbeat_internal]='metricbeat_writer'
 #	[filebeat_internal]='filebeat_writer'
 #	[heartbeat_internal]='heartbeat_writer'
@@ -35,6 +37,7 @@ users_roles=(
 declare -A roles_files
 roles_files=(
 	[logstash_writer]='logstash_writer.json'
+	[viewdata]="custom_user.json"
 #	[metricbeat_writer]='metricbeat_writer.json'
 #	[filebeat_writer]='filebeat_writer.json'
 #	[heartbeat_writer]='heartbeat_writer.json'
