6e13cd35ade97290ac59907d67d091ebcba38a58
ruben <uo276255@uniovi.es>
2024-04-16 15:38:25 +0200

    stating component testing

---
+++ webapp/src/__test__/components/Footer.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Footer from "../../views/components/Footer";
+import '@testing-library/jest-dom';
+
+describe("Footer component", () => {
+    test("renders current year and text", () => {
+        render(<Footer />);
+
+        const currentYear = new Date().getFullYear();
+        expect(screen.getByText(`© ${currentYear} ASW - WIQ05b`)).toBeInTheDocument();
+        expect(screen.getByRole("navigation")).toBeInTheDocument();
+        const appBar = screen.getByRole("navigation");
+        expect(appBar).toHaveStyle({ backgroundColor: "primary" });
+    });
+
+    test("has fixed position at the bottom of the page", () => {
+        render(<Footer />);
+        const appBar = screen.getByRole("navigation");
+        expect(appBar).toHaveStyle({ top: "auto", bottom: 0, position: "fixed" });
+    });
+
+    test("has centered text", () => {
+        render(<Footer />);
+        const toolbar = screen.getByRole("toolbar");
+        expect(toolbar).toHaveStyle({ justifyContent: "center" });
+    });
+});
