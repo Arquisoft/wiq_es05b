7a54b327b5da0a28456edbe056101af1e38c52f2
DavidAlvrz <davidalvarezm95@gmail.com>
2024-04-18 00:50:28 +0200

    Add friend tab component

--- webapp/src/views/Social.jsx
+++ webapp/src/views/Social.jsx
@@ -1,8 +1,45 @@
-import { Container } from "@mui/material";
+import { Container, Paper, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 import ProtectedComponent from "./components/ProtectedComponent";
+
+const Menu = (props) => {
+    const {setSelectedTab} = props;
+
+    return (
+        <Container sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+            <Paper elevation={3} sx={{ display: "flex", flexFlow: "column", gap: "1rem", justifyContent: "center", alignItems: "flex-start", padding: "2rem 1rem" }}>
+                <Typography variant="h6" element="p" sx={{ cursor: "pointer" }} onClick={() => setSelectedTab("friendsTab")}>Friends</Typography>
+                <Typography variant="h6" element="p" sx={{ cursor: "pointer" }} onClick={() => setSelectedTab("groupsTab")}>Groups</Typography>
+                <Typography variant="h6" element="p" sx={{ cursor: "pointer" }} onClick={() => setSelectedTab("addFriendTab")}>Add Friend</Typography>
+                <Typography variant="h6" element="p" sx={{ cursor: "pointer" }} onClick={() => setSelectedTab("joinGroupTab")}>Join Group</Typography>
+            </Paper>
+        </Container>
+    )
+}
+
+const tabStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+    padding: "2rem",
+    justifyContent: "center",
+    alignItems: "center"
+}
+
+const AddFriendTab = () => {
+    return (
+        <Container sx={tabStyle} >
+            <Typography variant="h4" element="p">Add Friends</Typography>
+            <TextField label="Search users..." variant="standard" />
+
+        </Container>
+    )
+}
 
 
 export default function Social() {
+    const [selectedTab, setSelectedTab] = useState("addFriendTab")
+
     return (
         <ProtectedComponent>
             <Container
@@ -10,10 +47,20 @@ export default function Social() {
                 sx={{
                     marginTop: 4,
                     display: "grid",
-                    gridTemplateColumns: "1fr 2fr"
+                    gridTemplateColumns: "1fr 2fr",
+                    height: "600px"
                 }}
             >
-                <h1>SOCIAL</h1>
+
+                <Menu setSelectedTab={setSelectedTab} />
+                <Paper  elevation={3} sx={{
+                    height: "100%",
+                }} >
+                    {selectedTab === "friendsTab" && <Typography variant="h6" element="p">Friends</Typography>}
+                    {selectedTab === "groupsTab" && <Typography variant="h6" element="p">Groups</Typography>}
+                    {selectedTab === "addFriendTab" && <AddFriendTab />}
+                    {selectedTab === "joinGroupTab" && <Typography variant="h6" element="p">Join Group</Typography>}
+                </Paper>
 
             </Container>
         </ProtectedComponent>
