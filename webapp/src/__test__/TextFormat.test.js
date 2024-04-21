d45a8921625efe0a626e7ee886245fb6efb99755
ruben <uo276255@uniovi.es>
2024-04-21 19:17:14 +0200

    Text format Test

---
+++ webapp/src/__test__/TextFormat.test.js
@@ -0,0 +1,38 @@
+const textFormat = require('../scripts/textFormat');
+
+describe('textFormat function', () => {
+    test('converts camel case to title case', () => {
+        const input = 'camelCaseExample';
+        const expectedOutput = 'Camel case example';
+        const result = textFormat(input);
+        expect(result).toEqual(expectedOutput);
+    });
+
+    test('converts snake case to title case', () => {
+        const input = 'snake_case';
+        const expectedOutput = 'Snake case';
+        const result = textFormat(input);
+        expect(result).toEqual(expectedOutput);
+    });
+
+    test('converts mixed case to title case', () => {
+        const input = 'Mixed_CamelCase snake_caseExample';
+        const expectedOutput = 'Mixed camel case snake_case example';
+        const result = textFormat(input);
+        expect(result).toEqual(expectedOutput);
+    });
+
+    test('handles single word input', () => {
+        const input = 'hello';
+        const expectedOutput = 'Hello';
+        const result = textFormat(input);
+        expect(result).toEqual(expectedOutput);
+    });
+
+    test('handles empty input', () => {
+        const input = '';
+        const expectedOutput = '';
+        const result = textFormat(input);
+        expect(result).toEqual(expectedOutput);
+    });
+});
