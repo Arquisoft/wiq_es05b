5af7927d58aae3551c92cafb8ed5ba22566bbf93
algarfer <algarferx@proton.me>
2024-04-23 19:32:30 +0200

    Added i18next common code to the npm package

--- cyt-utils/index.js
+++ cyt-utils/index.js
@@ -3,4 +3,6 @@ module.exports = {
   errorHandlerMiddleware: require('./logging/middleware/ErrorHandlerMiddleware'),
   requestLoggerMiddleware: require('./logging/middleware/ReqLoggerMiddleware'),
   responseLoggerMiddleware: require('./logging/middleware/ResLoggerMiddleware'),
+  i18nextInitializer: require('./i18n/i18nextInitializer'),
+  i18nextMiddleware: require('./i18n/i18nextMiddleware'),
 }
