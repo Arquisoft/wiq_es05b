36e40809612acf2bcbf4250c93d81358fcd9bfb4
algarfer <algarferx@proton.me>
2024-04-28 22:09:57 +0200

    Try to fix sonar v3

---
+++ jordi/sonarfix.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const {readFileSync, writeFileSync} = require('fs')
+
+const lcovFile = path.resolve(__dirname, './coverage/lcov.info')
+const rawFile = readFileSync(lcovFile, 'utf8')
+const rebuiltPaths = rawFile.split('\n').map( singleLine => {
+  if (singleLine.startsWith('SF:')) {
+    return singleLine.replace('SF:', `SF:${__dirname}/`)
+  }
+  return singleLine
+}).join('\n')
+console.log(rebuiltPaths)
+writeFileSync(lcovFile, rebuiltPaths, 'utf8')
