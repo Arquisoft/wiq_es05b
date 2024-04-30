62689183e1c7f1faae283ef329ae2c79d0c19c1d
algarfer <algarferx@proton.me>
2024-04-30 17:32:42 +0200

    Fixed e2e

--- gatewayservice/routes/authRoutes.js
+++ gatewayservice/routes/authRoutes.js
@@ -5,19 +5,13 @@ module.exports = (app, axios) => {
   app.post("/login", async (req, res, next) => {
     axios.post(`${authServiceUrl}/login`, req.body)
       .then(response => res.json(response.data))
-      .catch(e => {
-        if(e.code) return next(e.code)
-        next({status: e.response.status, error: e.response.data})
-      });
+      .catch(e => next({status: e.response.status, ...e.response.data}));
   });
 
   app.get("/validate/:token", (req, res, next) => {
     axios
       .get(`${authServiceUrl}/validate/${req.params.token}`)
       .then(({ data }) => res.json({valid: data.valid}))
-      .catch(e => {
-        if(e.code) return next(e.code)
-        next({status: e.response.status, error: e.response.data})
-      });
+      .catch(e => next({status: e.response.status, ...e.response.data}));
   });
 };
